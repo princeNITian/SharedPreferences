72bd0a3f791463c608d6ba1e0b7a1415935b0ff3
Prince Raj <princeraj427@gmail.com>
2019-03-21 16:07:11 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.prince.hsamp12;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvWelcome;
+    EditText etName;
+    Button btnSubmit;
+
+    public static final String MY_PREFS_FILENAME="com.example.prince.hsamp12.Names";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvWelcome = (TextView)findViewById(R.id.tvWelcome);
+        etName = (EditText) findViewById(R.id.etName);
+        btnSubmit = (Button)findViewById(R.id.btnSubmit);
+
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_FILENAME,MODE_PRIVATE);
+        String user = prefs.getString("user","");
+        tvWelcome.setText("Welcome to my app "+user+" !");
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etName.getText().toString().trim();
+                tvWelcome.setText("Welcome to my app "+name+" !");
+
+                SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_FILENAME,MODE_PRIVATE).edit();
+                editor.putString("user",name);
+                editor.commit();
+            }
+        });
+    }
+}
